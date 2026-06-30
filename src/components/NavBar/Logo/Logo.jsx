5bfd4ae3129d4ce67f977b7ac0c082a7bad48c32
Maximiliano Drago <maximilianoadrago@gmail.com>
2026-06-30 12:59:37 -0300

    fix logo path for gh-pages

--- src/components/NavBar/Logo/Logo.jsx
+++ src/components/NavBar/Logo/Logo.jsx
@@ -7,7 +7,7 @@ function Logo() {
       <div className="logo-container">
         <NavLink to="/" className="logo">
           {/* Asegúrate de colocar tu imagen del logo en la carpeta public como logo.png o logo.jpg y actualizar esta ruta si es necesario */}
-          <img src="/logo.png" alt="Naturaleza Viva" className="logo-img" />
+          <img src={`${import.meta.env.BASE_URL}logo.png`} alt="Naturaleza Viva" className="logo-img" />
         </NavLink>
       </div>
     </>
